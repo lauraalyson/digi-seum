7257daa7f12108078b6e67cd1c3e56dc43d7db9e
lauraalyson <lauraalyson3@gmail.com>
2021-09-17 12:04:11 -0400

    saving url from img

--- src/components/artwork/Canvas.js
+++ src/components/artwork/Canvas.js
@@ -1,36 +1,85 @@
 import { React, Component } from 'react'
 import { withRouter } from 'react-router-dom'
-import { Stage, Layer } from 'react-konva'
+import { Layer, Rect, Stage } from 'react-konva'
 // import ArtworkCanvas from './ArtworkCanvas'
 
-class Canvas extends Component {
-handleExportClick = () => {
-  console.log(
-    this.stageRef.getStage().toDataURL({ mimeType: 'image/png', quality: 1 }))
-}
+class MyRect extends Component {
+  constructor (...args) {
+    super(...args)
+    this.state = {
+      color: 'green'
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
 
-render () {
-  return (
-    <div>
-      <Stage
-        width={700}
-        height={700}
-        ref={(node) => {
-          this.stageRef = node
-        }}
-      >
-        <Layer>
-          {/* <ArtworkCanvas /> */}
-        </Layer>
-      </Stage>
-      <button
-        style={{ position: 'absolute', top: '0' }}
-        onClick={this.handleExportClick}>
-                Export stage
-      </button>
-    </div>
-  )
+  handleClick () {
+    this.setState({
+      color: 'red'
+    })
+  }
+
+  render () {
+    return (
+      <Rect
+        x={10}
+        y={10}
+        width={50}
+        height={50}
+        fill={this.state.color}
+        shadowBlur={10}
+        onClick={this.handleClick}
+      />
+    )
+  }
 }
+class Canvas extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      url: ''
+    }
+  }
+
+    handleExportClick = (event) => {
+      console.log('this is the event \n', event)
+      // console.log(this.stageRef.getStage().toDataURL({ mimeType: 'image/png' }))
+      const imgUrl = this.stageRef
+        .getStage()
+        .toDataURL({ mimeType: 'image/png' })
+
+      console.log('this is imgURL \n', imgUrl)
+      this.setState({ url: imgUrl })
+      console.log('this is now the state of the url', this.state.url)
+    }
+
+    // handleShowImage = (event) => {
+
+    // }
+
+    render () {
+      return (
+        <div>
+          <Stage
+            width={700}
+            height={700}
+            ref={(node) => {
+              this.stageRef = node
+            }}>
+            <Layer>
+              <MyRect />
+              {/* <ArtworkCanvas /> */}
+            </Layer>
+          </Stage>
+          <button
+            style={{ position: 'absolute', top: '0' }}
+            onClick={this.handleExportClick}>
+                Export stage
+          </button>
+          <p><img src={this.state.url}/>{this.state.url}</p>
+        </div>
+      )
+    }
 }
 
 export default withRouter(Canvas)
--- src/components/artwork/CreateArtwork.js
+++ src/components/artwork/CreateArtwork.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom'
 
 import { createArtwork } from '../../api/artwork'
+import Canvas from '../artwork/Canvas'
 
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
@@ -13,7 +14,8 @@ class CreateArtwork extends Component {
     this.state = {
       title: '',
       description: '',
-      img: ''
+      img: '',
+      imgUrl: ''
     }
   }
 
@@ -21,6 +23,10 @@ handleChange = (event) =>
   this.setState({
     [event.target.name]: event.target.value
   })
+
+onFindUrl = (event) => {
+  this.setState({ imgUrl: this.state.url })
+}
 
 onCreateArtwork = (event) => {
   event.preventDefault()
@@ -43,7 +49,7 @@ render () {
     <div className='row'>
       <div className='col-sm-10 col-sm-8 mx-auto mt-5'>
         <Form onSubmit={this.onCreateArtwork}>
-
+          <Canvas onClick={this.onFindUrl}/>
           <Form.Group controlId='title'>
             <Form.Label>Title</Form.Label>
             <Form.Control
